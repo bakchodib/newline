85549ebcdc6aa5d056a326138deff36cd7a236d9
Bakchodi Baba <BakchodiB@gmail.com>
2025-07-19 20:53:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,23 @@
 
+// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
-// IMPORTANT: Replace this with your own Firebase project configuration
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCL77cBUaywr84qDscxCyy0PM_gRnxp7p0",
+  authDomain: "jlsfinalhai.firebaseapp.com",
+  projectId: "jlsfinalhai",
+  storageBucket: "jlsfinalhai.appspot.com",
+  messagingSenderId: "90437797359",
+  appId: "1:90437797359:web:ee86797f9863e64a7ea21a",
+  measurementId: "G-ZSV85HDRT1"
 };
 
 // Initialize Firebase
@@ -18,4 +25,8 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db };
+// Initialize Analytics only if it's supported
+const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+
+
+export { app, auth, db, analytics };
